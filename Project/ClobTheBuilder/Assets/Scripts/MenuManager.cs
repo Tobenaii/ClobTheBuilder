7f484ff6cdc168569d81000562f8ab86ea462cfc
BlakeKolodziej <blakekolodziej@gmail.com>
2019-06-27 15:21:08 +1000

    Created main menu functionality

---
+++ Project/ClobTheBuilder/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    //Menu holders
+    public GameObject mainMenuHolder;
+    public GameObject levelSelectHolder;
+
+
+    public void Awake()
+    {
+        mainMenuHolder.SetActive(true);
+        levelSelectHolder.SetActive(false);
+    }
+
+    public void Play()
+    {
+        mainMenuHolder.SetActive(false);
+        levelSelectHolder.SetActive(true);
+    }
+
+    public void Back()
+    {
+        mainMenuHolder.SetActive(true);
+        levelSelectHolder.SetActive(false);
+    }
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(2, LoadSceneMode.Single);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(3, LoadSceneMode.Single);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(4, LoadSceneMode.Single);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
