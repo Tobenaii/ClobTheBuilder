e80a79ffa2460cf283a9f1c55bceaa37386574c7
Tobenaii <akuapengin42@gmail.com>
2019-06-27 11:00:57 +1000

    Items and inventory

---
+++ Project/ClobTheBuilder/Assets/Scripts/Data/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/Inventory")]
+public class Inventory : ScriptableObject
+{
+    [System.Serializable]
+    public class InventoryData
+    {
+        public ItemData item;
+        public int itemAmmount;
+    }
+
+    public List<InventoryData> inventoryItems;
+}
---
+++ Project/ClobTheBuilder/Assets/Scripts/Data/ItemData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = ("Item/Item"))]
+public class ItemData : ScriptableObject
+{
+    public GameObject itemPrefab;
+}
