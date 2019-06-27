1bbc9bb21e8e40e98fb5c4c88f091fe443cb9188
Tobenaii <akuapengin42@gmail.com>
2019-06-27 11:15:14 +1000

    Inventory

---
+++ Project/ClobTheBuilder/Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    [SerializeField]
+    private Inventory m_inventory;
+    [SerializeField]
+    private List<GameObject> m_slots;
+
+    private void OnEnable()
+    {
+        int index = 0;
+        foreach (GameObject slot in m_slots)
+        {
+            if (index >= m_inventory.inventoryItems.Count)
+                return;
+            Instantiate(m_inventory.inventoryItems[index].item, slot.transform, false);
+            index++;
+        }
+    }
+}
