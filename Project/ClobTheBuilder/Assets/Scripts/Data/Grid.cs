aac857c6494a893ca6252afaa29746c1e62d08cf
Tobenaii <akuapengin42@gmail.com>
2019-06-27 10:50:29 +1000

    Basic grid

---
+++ Project/ClobTheBuilder/Assets/Scripts/Data/Grid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = ("Grid"))]
+public class Grid : ScriptableObject
+{
+    [SerializeField]
+    private Vector2 m_gridSize;
+
+    public Vector3 SnapToGrid(Vector3 pos)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 20, 1 << 9))
+            return new Vector3((int)hit.point.x, (int)hit.point.y, pos.z);
+        return pos;
+    }
+}
---
+++ Project/ClobTheBuilder/Assets/Scripts/Item.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private Renderer m_renderer;
+    [SerializeField]
+    private float m_ghostAlpha;
+    [SerializeField]
+    private Grid m_grid;
+    private bool m_isGhost;
+
+    private void OnEnable()
+    {
+        Ghostify();
+    }
+
+    private void Ghostify()
+    {
+        Color defaultCol = m_renderer.material.color;
+        defaultCol.a = m_ghostAlpha;
+        m_renderer.material.color = defaultCol;
+        m_isGhost = true;
+    }
+
+    private void Unghostify()
+    {
+        Color defaultCol = m_renderer.material.color;
+        defaultCol.a = 1.0f;
+        m_renderer.material.color = defaultCol;
+        m_isGhost = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_isGhost)
+            return;
+        Debug.Log(Camera.main.orthographicSize);
+
+        transform.position = m_grid.SnapToGrid(transform.position);
+        if (Input.GetMouseButtonDown(0))
+            Unghostify();
+    }
+}
